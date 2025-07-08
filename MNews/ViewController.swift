a19f68d6ccd3b3b9cbde4ae7dace8a6e489b8a6f
muhammadmajd <muhammad.aliah23434@gmail.com>
2025-07-08 23:48:17 +0300

    Initial Commit

---
+++ MNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MNews
+//
+//  Created by muhammad on 08/07/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
